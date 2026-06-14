c8280fe8be2d36e2bbbb056cd1483724f170296b
AayushBade14 <aayushrajubade@gmail.com>
2026-06-14 11:46:06 +0530

    Basic dependency setup

--- App/src/Application.cpp
+++ App/src/Application.cpp
@@ -1,8 +1,31 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main(void)
+int main(void) 
 {
-	std::cout << "Hello, World!" << std::endl;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "ConvexHull", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	while (!glfwWindowShouldClose(window)) 
+	{
+		glfwPollEvents();
+
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
 
 	return 0;
 }
